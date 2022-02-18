6cb57a3b95fa8235e3d2b2c5d3c3d179888dcd6a
dirtycleanjiejie <dirtycleanjiejie@gmail.com>
2022-02-18 17:39:13 +0800

    feat: 新增d3-demo

---
+++ src/components/d3-charts/axis.vue
@@ -0,0 +1,16 @@
+<template>123</template>
+<script setup lang="ts">
+import * as d3 from 'd3'
+const width = 600
+const height = 600
+// 创建svg画布
+const svg = d3.select('body').append('svg').attr('width', width).attr('height', height)
+// 定义用于坐标轴的线性比例尺
+const xScale = d3.scaleLinear().domain([0, 10]).range([0, 300])
+// 定义坐标轴 刻度方向朝下
+const axis = d3.axisBottom(xScale)
+// 在svg中添加一个包含坐标轴各元素的g元素
+const gAxis = svg.append('g').attr('transform', 'translate(80,80)').attr('class', 'axis')
+// 在gAxis中绘制坐标轴
+gAxis.call(axis)
+</script>
---
+++ src/views/home/d3Demo.vue
@@ -0,0 +1,8 @@
+<template>
+    <Ring />
+    <Axis />
+</template>
+<script lang="ts" setup>
+import Ring from '@/components/d3-charts/ring.vue'
+import Axis from '@/components/d3-charts/axis.vue'
+</script>
